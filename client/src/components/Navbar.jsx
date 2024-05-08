e897bd552204565035c5acb62ab5b489179edc43
MikiZenebe <mickyaszenebe@gmail.com>
2024-05-07 22:08:50 -0500

    navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,3 +1,34 @@
+import {
+  BtnContainer,
+  Logo,
+  Nav,
+  NavContainer,
+  NavItems,
+  NavLogo,
+  Navlink,
+} from "../styles/Nav";
+import Button from "./Button";
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <Nav>
+      <NavContainer>
+        <NavLogo>
+          <Logo />
+        </NavLogo>
+
+        <NavItems>
+          <Navlink to="/">Home</Navlink>
+          <Navlink to="/shop">Shop</Navlink>
+          <Navlink to="/newArrivals">New Arrivals</Navlink>
+          <Navlink to="/orders">Orders</Navlink>
+          <Navlink to="/contact">Contact</Navlink>
+        </NavItems>
+
+        <BtnContainer>
+          <Button text="SignIn" small />
+        </BtnContainer>
+      </NavContainer>
+    </Nav>
+  );
 }
---
+++ client/src/styles/Nav.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+
+export const Nav = styled.div``;
+export const NavContainer = styled.div``;
+export const NavLogo = styled.div``;
+export const Logo = styled.img``;
+export const NavItems = styled.div``;
+export const Navlink = styled(NavLink)``;
+export const BtnContainer = styled.div``;
